d0612f4417b0ac29b3a09266a7b78d38a8722211
Yejee Arin Choi <yej.arin.choi@gmail.com>
2017-11-13 22:47:53 -0500

    Test rawScore function

--- app/bowlingGame_spec.js
+++ app/bowlingGame_spec.js
@@ -6,7 +6,6 @@ describe("First test", function() {
 
 describe("#allTurns", function() {
   let scores, bowlingGame;
-
   beforeEach(function(){
     scores = "5- X 8/"
     bowlingGame = new BowlingGame(scores)
@@ -16,7 +15,34 @@ describe("#allTurns", function() {
     expect(returnValues instanceof Array).toBeTruthy();
   });
   it("should return represent each turn", function() {
-    expect(bowlingGame.allTurns().length).toEqual(3)
-    expect(bowlingGame.allTurns()).toEqual(['5-', 'X', '8/'])
+    expect(bowlingGame.allTurns().length).toEqual(3);
+    expect(bowlingGame.allTurns()).toEqual(['5-', 'X', '8/']);
+  });
+})
+
+describe("#rawScores", function() {
+  let scores, bowlingGame, rawScores;
+  beforeEach(function(){
+    scores = "25 5- X 8/"
+    bowlingGame = new BowlingGame(scores)
+    rawScores = bowlingGame.rawScores();
+  });
+
+  it("returns an array", function() {
+    expect(rawScores instanceof Array).toBeTruthy();
+  });
+  it("should return a raw string concat of both bowls", function() {
+    expect(rawScores[0]).toEqual("25")
+  });
+  it("should return a raw string concat of both bowls when one is miss", function() {
+    debugger
+    bowlingGame
+    expect(rawScores[1]).toEqual("50")
+  });
+  it("should return a raw string X", function() {
+    expect(rawScores[2]).toEqual("X")
+  });
+  it("should return a raw string concat of number plus spare remainder", function() {
+    expect(rawScores[3]).toEqual("82")
   });
 })
