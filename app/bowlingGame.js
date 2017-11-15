2a5cb8f1783ac6b3385f7ee67b43a1b1b9b06c17
Yejee Arin Choi <yej.arin.choi@gmail.com>
2017-11-14 20:13:04 -0500

    Add evaluateStrike and evaluateSpare functions

--- app/bowlingGame.js
+++ app/bowlingGame.js
@@ -41,3 +41,43 @@ function formatSpares(turn) {
   let spareScore = 10 - (+turn[0])
   return rawScore += spareScore
 }
+
+
+BowlingGame.prototype.evaluateStrike = function(idx, rawScores) {
+  let next = rawScores[idx + 1] || "0"
+  let nextNext = rawScores[idx + 2] || "0"
+  if (next.length === 1) {
+    let nextSum = determineNextTwoTurns(next, nextNext)
+    return (10 + nextSum)
+  } else if (!next) {
+    return 10
+  } else {
+    return 10 + (+next[0]) + (+next[1])
+  }
+}
+
+BowlingGame.prototype.evaluateSpare = function(idx, rawScores) {
+  let next = rawScores[idx + 1] || "0"
+  let nextVal;
+  if (next === "X") {
+    nextVal = 10
+  } else {
+    nextVal = next[0] || "0"
+  }
+  return 10 + (+nextVal)
+}
+
+function determineNextTwoTurns(next, nextNext) {
+  let nextVal, nextNextVal
+  if (next[0] === "X") {
+    nextVal = 10
+  } else {
+    nextVal = +next[0]
+  }
+  if (nextNext[0] === "X") {
+    nextNextVal = 10
+  } else {
+    nextNextVal = +nextNext[0]
+  }
+  return nextVal + nextNextVal
+}
